8ef35ed5feb90920a16d8d141624f9523d108d0d
VLadimir <53127805+Xtreller@users.noreply.github.com>
2019-07-24 17:32:01 +0300

    Add files via upload

---
+++ Stealer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Spy spy = new Spy();
+            string res = spy.CollectGettersAndSetters("Hacker");
+            Console.WriteLine(res);
+        }
+    }
+}
